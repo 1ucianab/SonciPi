13256e52ee9bc7878a67d7b9fbfc62be416833e3
1ucianab <160068922+1ucianab@users.noreply.github.com>
2024-03-19 11:40:14 -0700

    Update DreamsFleetwoodMac.rb

--- DreamsFleetwoodMac.rb
+++ DreamsFleetwoodMac.rb
@@ -3,29 +3,31 @@ use_synth :piano
 s = 1.5
 
 #repeats throughout song
-play :f3, sustain: s
-play :a3, sustain: s
-play :c4, sustain: s
-
-sleep 2
-
-play :f3, sustain: s
-play :a3, sustain: s
-play :c4, sustain: s
-
-sleep 2
-
-play :g3, sustain: s
-play :b3, sustain: s
-play :d4, sustain: s
-
-sleep 2
-
-play :g3, sustain: s
-play :b3, sustain: s
-play :d4, sustain: s
-
-sleep 2
+define :back do
+  play :f3, sustain: s
+  play :a3, sustain: s
+  play :c4, sustain: s
+  
+  sleep 2
+  
+  play :f3, sustain: s
+  play :a3, sustain: s
+  play :c4, sustain: s
+  
+  sleep 2
+  
+  play :g3, sustain: s
+  play :b3, sustain: s
+  play :d4, sustain: s
+  
+  sleep 2
+  
+  play :g3, sustain: s
+  play :b3, sustain: s
+  play :d4, sustain: s
+  
+  sleep 2
+end
 # end of repeating segment
 
 play :f3, sustain: s
@@ -44,3 +46,6 @@ play :g3, sustain: 3
 play :b3, sustain: 3
 play :d4, sustain: 3
 
+sleep 3
+
+back
