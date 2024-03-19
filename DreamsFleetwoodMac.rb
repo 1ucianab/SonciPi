6f20bba8b51c971920a206af08485dc8784dde61
1ucianab <160068922+1ucianab@users.noreply.github.com>
2024-03-19 11:33:17 -0700

    Create DreamsFleetwoodMac.rb

---
+++ DreamsFleetwoodMac.rb
@@ -0,0 +1,46 @@
+use_bpm 110
+use_synth :piano
+s = 1.5
+
+#repeats throughout song
+play :f3, sustain: s
+play :a3, sustain: s
+play :c4, sustain: s
+
+sleep 2
+
+play :f3, sustain: s
+play :a3, sustain: s
+play :c4, sustain: s
+
+sleep 2
+
+play :g3, sustain: s
+play :b3, sustain: s
+play :d4, sustain: s
+
+sleep 2
+
+play :g3, sustain: s
+play :b3, sustain: s
+play :d4, sustain: s
+
+sleep 2
+# end of repeating segment
+
+play :f3, sustain: s
+play :a3, sustain: s
+play :c4, sustain: s
+
+sleep 2
+
+play :f3, sustain: s
+play :a3, sustain: s
+play :c4, sustain: s
+
+sleep 2
+
+play :g3, sustain: 3
+play :b3, sustain: 3
+play :d4, sustain: 3
+
