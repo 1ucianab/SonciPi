37078b2c251cae908c6c99ff915cf46e3be7cf73
1ucianab <160068922+1ucianab@users.noreply.github.com>
2024-03-26 10:42:13 -0700

    Create DreamsFleetwoodMacLyrics.rb

---
+++ DreamsFleetwoodMacLyrics.rb
@@ -0,0 +1,84 @@
+use_synth :piano
+use_bpm 120
+s = 1
+
+play :a4, sustain: 1.6
+sleep s
+play :c5
+sleep s
+play :d5, sustain: 1.6
+sleep s
+play :e5, sustain: 2
+sleep s
+play :d5
+sleep s
+play :c5, sustain: 1.6
+sleep s
+play :a4
+sleep s
+play :c5
+sleep s
+play :d5, sustain: 2
+sleep s
+play :a4
+sleep s
+play :c5, sustain: 1
+sleep s
+play :a4
+sleep s
+play :d5, sustain: 2
+sleep s
+play :a4, sustain: 2.5
+sleep 3
+# pause
+play :a4, sustain: 1
+sleep s
+play :c5
+sleep s
+play :d5, sustain: 1
+sleep s
+play :e5, sustain: 1.6
+sleep s
+play :d5
+sleep s
+play :c5, sustain: 1
+sleep s
+play :d5
+sleep s
+play :g4
+sleep s
+play :a4, sustain: 3
+sleep 5
+#higher pitch
+play :e5
+sleep s
+play :a5, sustain: 1
+sleep s
+play :g5
+sleep s
+play :c6
+sleep s
+play :b5, sustain: 1.5
+sleep s
+play :a5
+sleep s
+play :a5, sustain: 1
+sleep s
+play :g5, sustain: 1
+sleep s
+play :d5
+sleep s
+play :e5, sustain: 1
+sleep s
+play :e5
+sleep s
+play :e5, sustain: 1
+sleep s
+play :e5, sustain: 1
+sleep s
+play :e5, sustain: 1
+sleep s
+play :a4, sustain: 3
+sleep s
+play :a4
+sleep s
