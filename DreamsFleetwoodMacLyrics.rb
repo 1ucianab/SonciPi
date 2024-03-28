b5dab88cc8d501f8b4f2f72fd2e05a2e0ff06ec5
1ucianab <160068922+1ucianab@users.noreply.github.com>
2024-03-28 11:40:01 -0700

    Update DreamsFleetwoodMacLyrics.rb

--- DreamsFleetwoodMacLyrics.rb
+++ DreamsFleetwoodMacLyrics.rb
@@ -5,41 +5,42 @@ s = 1
 ilyrics = [:a4, :c5, :d5, :e5, :d5, :c5, :a4, :c5, :d5, :a4, :c5, :a4, :d5, :a4,]
 i = 0
 sus = [1.6, 0, 1.6, 2, 0, 1.6, 0, 0, 2, 0, 1, 0, 2, 2.5]
+imimi = [1, 0.5, 1, 1, 0.5, 1, 1, 0.5, 1, 0.5, 1, 0.5, 1, 1]
 
 14.times do
   play ilyrics[i], sustain: sus[i]
-  sleep s
+  sleep imimi[i]
   i = i + 1
 end
 =end
 
 play :a4, sustain: 1.6
-sleep s
+sleep 1
 play :c5
-sleep s
+sleep 0.5
 play :d5, sustain: 1.6
-sleep s
-play :e5, sustain: 2
-sleep s
-play :d5
-sleep s
-play :c5, sustain: 1.6
-sleep s
+sleep 1
+play :e5, sustain: 2 #go
+sleep 1
+play :d5 #ag
+sleep 0.5
+play :c5, sustain: 1.6 # ain
+sleep 1
+play :a4 #you
+sleep 1
+play :c5 #say
+sleep 0.5
+play :d5, sustain: 2 #you
+sleep 1
+play :a4 #want
+sleep 1
+play :c5, sustain: 1 #your
+sleep 0.5
 play :a4
-sleep s
-play :c5
-sleep s
-play :d5, sustain: 2
-sleep s
-play :a4
-sleep s
-play :c5, sustain: 1
-sleep s
-play :a4
-sleep s
-play :d5, sustain: 2
-sleep s
-play :a4, sustain: 2.5
+sleep 1
+play :d5, sustain: 2 #free-
+sleep 1
+play :a4, sustain: 2.5 #dom
 sleep 3
 # pause
 =begin
@@ -195,4 +196,41 @@ sleep s
 play :g4
 sleep s
 play :g4
+sleep s
+
+=begin
+qlyrics = [:e5,:d5, :c5, :g4, :e5, :d5, :d5, :c5, :c5, :d5, :d5, :c5]
+q = 0
+qsus = [1.5, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 3]
+
+12.times do
+  play qlyrics[q]
+  sleep s
+  q = q + 1 
+end
+=end
+
+play :e5, sustain: 1.5
+sleep s
+play :d5, sustain: 1
+sleep s
+play :c5, sustain: 1
+sleep s
+play :g4
+sleep s
+play :e5
+sleep s
+play :d5
+sleep s
+play :d5, sustain: 1
+sleep s
+play :c5
+sleep s
+play :c5
+sleep s
+play :d5
+sleep s
+play :d5, sustain: 1
+sleep s
+play :c5, sustain: 3
 sleep s
