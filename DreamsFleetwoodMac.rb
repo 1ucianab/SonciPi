d1aa00a40d811957b45f72c5f156053509032d47
1ucianab <160068922+1ucianab@users.noreply.github.com>
2024-04-15 12:18:25 -0700

    Update DreamsFleetwoodMac.rb

--- DreamsFleetwoodMac.rb
+++ DreamsFleetwoodMac.rb
@@ -9,24 +9,32 @@ freedom = "C:/Users/luciana_brennan/Desktop/you_want_your_freedom.mp3"
 ilyrics = [:a4, :c5, :d5, :e5, :d5, :c5, :a4, :c5, :d5, :a4, :c5, :a4, :d5, :a4,]
 i = 0
 sus = [1.6, 0, 1.6, 2, 0, 1.6, 0, 0, 2, 0, 1, 0, 2, 2.5]
-is = [1, 0.5, 1, 1, 0.5, 1, 1, 0.5, 1, 0.5, 1, 1, 1]
+imimi = [1, 0.5, 1, 1, 0.5, 1, 1, 0.5, 1, 1, 1, 0.5, 1, 3]
 
 blyrics = [:a4,:c5,:d5,:e5,:d5,:c5,:d5,:g4,:a4]
 b = 0
 bsus = [1, 0, 1, 1.6, 0, 1, 0, 0, 3]
+bmimi = [1, 0.5, 1, 1, 0.5, 1, 0.5, 0.5, 5]
 
-hlyrics = [:e5,:a5, :g5, :c6, :b5, :a5, :a5, :g5, :d5, :e5, :e5, :e5, :e5, :e5, :a4, :a4]
+hlyrics = [:e5,:a5, :g5, :c6, :b5, :a5, :a5, :g5, :d5, :e5, :e5, :e5, :e5, :a4, :a4]
 h = 0
-hsus = [0, 1, 0, 0, 1.5, 0, 1, 1, 0, 1, 0, 1, 1, 1, 3, 0]
+hsus = [0, 1, 0, 0, 1.5, 0, 1, 1, 0, 1, 0, 1, 1, 3, 0]
+hmimi = [1, 0.5, 1, 0.5, 1, 1, 0.5, 1, 1, 1, 0.5, 1, 1, 2, 1]
 
 sblyrics = [:d5, :e5, :d5, :c5, :a4, :d5, :e5, :d5, :c5, :a4, :d5, :c5, :g4, :g4]
 sb = 0
 sbsus = [0, 1, 0, 1, 0, 0, 0, 1.5, 0, 0, 1, 3, 0, 0]
+sbmimi = [0.5, 1, 0.5, 1, 1, 0.5, 1, 1, 0.5, 0.5, 1, 0.5, 2, 1]
 
 brlyrics = [:e5, :d5, :c5, :c5, :a4, :d5, :c5, :c5, :d5, :d5, :c5, :g4, :g4]
 br = 0
 brsus = [1.5, 1, 1, 0, 0, 1 ,1, 1, 0, 1, 3, 0, 0]
+brmimi = [1, 0.5, 1, 1, 0.5, 1, 0.5, 1, 0.5, 1, 1, 0.5, 1]
 
+qlyrics = [:e5,:d5, :c5, :g4, :e5, :d5, :d5, :c5, :c5, :d5, :d5, :c5]
+q = 0
+qsus = [1.5, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 3]
+qmimi = [1, 0.5, 0.5,0.5, 1, 0.5, 0.5, 0.5, 0.5, 0.5, 1, 1]
 
 #repeats throughout song
 define :back do |n, n2, n3, s|
@@ -60,7 +68,7 @@ end
 
 14.times do
   play ilyrics[i], sustain: sus[i]
-  sleep s
+  sleep imimi[i]
   i = i + 1
 end
 
@@ -68,7 +76,7 @@ sleep 3
 
 9.times do
   play blyrics[b], sustain: bsus[b]
-  sleep s
+  sleep bmimi[b]
   b = b + 1
 end
 
@@ -76,7 +84,7 @@ sleep 3
 
 16.times do
   play hlyrics[h], sustain: hsus[h]
-  sleep s
+  sleep hmimi[h]
   h = h + 1
 end
 
@@ -84,7 +92,7 @@ sleep 3
 
 14.times do
   play sblyrics[sb], sustain: sbsus[sb]
-  sleep s
+  sleep sbmimi[sb]
   sb = sb + 1
 end
 
@@ -92,6 +100,12 @@ sleep 3
 
 13.times do
   play brlyrics[br], sustain: brsus[br]
-  sleep s
+  sleep brmimi[br]
   br = br + 1
+end
+
+12.times do
+  play qlyrics[q]
+  sleep qmimi[q]
+  q = q + 1 
 end
