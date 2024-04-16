34b5806819ad16c3656b4035ee46f5dfc8e44e2d
1ucianab <160068922+1ucianab@users.noreply.github.com>
2024-04-16 11:39:45 -0700

    Update DreamsFleetwoodMac.rb

--- DreamsFleetwoodMac.rb
+++ DreamsFleetwoodMac.rb
@@ -1,9 +1,13 @@
+#https://www.youtube.com/watch?v=xRf153J3VhQ
+#https://www.youtube.com/watch?v=Q2Qjf3wCgCo
 
 use_bpm 120
 use_synth :piano
 s = 1
 
 freedom = "C:/Users/luciana_brennan/Desktop/you_want_your_freedom.mp3"
+down = "C:/Users/luciana_brennan/Desktop/keep_you_down.mp3"
+g1 = "C:/Users/luciana_brennan/Desktop/guitar_1.mp3"
 
 
 ilyrics = [:a4, :c5, :d5, :e5, :d5, :c5, :a4, :c5, :d5, :a4, :c5, :a4, :d5, :a4,]
@@ -16,38 +20,26 @@ b = 0
 bsus = [1, 0, 1, 1.6, 0, 1, 0, 0, 3]
 bmimi = [1, 0.5, 1, 1, 0.5, 1, 0.5, 0.5, 5]
 
-hlyrics = [:e5,:a5, :g5, :c6, :b5, :a5, :a5, :g5, :d5, :e5, :e5, :e5, :e5, :a4, :a4]
-h = 0
-hsus = [0, 1, 0, 0, 1.5, 0, 1, 1, 0, 1, 0, 1, 1, 3, 0]
-hmimi = [1, 0.5, 1, 0.5, 1, 1, 0.5, 1, 1, 1, 0.5, 1, 1, 2, 1]
-
-sblyrics = [:d5, :e5, :d5, :c5, :a4, :d5, :e5, :d5, :c5, :a4, :d5, :c5, :g4, :g4]
-sb = 0
-sbsus = [0, 1, 0, 1, 0, 0, 0, 1.5, 0, 0, 1, 3, 0, 0]
-sbmimi = [0.5, 1, 0.5, 1, 1, 0.5, 1, 1, 0.5, 0.5, 1, 0.5, 2, 1]
-
-brlyrics = [:e5, :d5, :c5, :c5, :a4, :d5, :c5, :c5, :d5, :d5, :c5, :g4, :g4]
-br = 0
-brsus = [1.5, 1, 1, 0, 0, 1 ,1, 1, 0, 1, 3, 0, 0]
-brmimi = [1, 0.5, 1, 1, 0.5, 1, 0.5, 1, 0.5, 1, 1, 0.5, 1]
-
-qlyrics = [:e5,:d5, :c5, :g4, :e5, :d5, :d5, :c5, :c5, :d5, :d5, :c5]
-q = 0
-qsus = [1.5, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 3]
-qmimi = [1, 0.5, 0.5,0.5, 1, 0.5, 0.5, 0.5, 0.5, 0.5, 1, 1]
+t = 0
+tlyric = [:e5, :e5, :e5, :e5, :d5, :e5, :d5, :c5, :c5, :c5, :d5, :a4]
+tsus = [1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 3]
+tmimi = [1, 0.5, 1, 0.5, 1, 1, 0.5, 1, 0.5, 1, 1]
 
 #repeats throughout song
 define :back do |n, n2, n3, s|
-  play n, sustain: s
-  play n2, sustain: s
-  play n3, sustain: s
+  play n, sustain: s, amp: 0.5
+  play n2, sustain: s, amp: 0.5
+  play n3, sustain: s, amp: 0.5
   sleep 2
-  play n, sustain: s
-  play n2, sustain: s
-  play n3, sustain: s
+  play n, sustain: s, amp: 0.5
+  play n2, sustain: s, amp: 0.5
+  play n3, sustain: s, amp: 0.5
   sleep 2
 end
 # end of repeating segment
+
+sample g1, amp: 2
+sleep 8
 
 back :f3, :a3, :c4, 1.6
 
@@ -70,6 +62,10 @@ end
   play ilyrics[i], sustain: sus[i]
   sleep imimi[i]
   i = i + 1
+  
+  if (i == 9)
+    sample freedom
+  end
 end
 
 sleep 3
@@ -78,10 +74,22 @@ sleep 3
   play blyrics[b], sustain: bsus[b]
   sleep bmimi[b]
   b = b + 1
+  
+  if (b == 5)
+    sample down
+  end
 end
 
 sleep 3
+#add a whooshing almost wind like sound then go fade into oohh thunder always happens when its raining
+#from there then do an ooowhooowhooo
 
+12.times do
+  play tlyric[t], sustain: tsus[t]
+  sleep tmimi[t]
+end
+
+=begin
 16.times do
   play hlyrics[h], sustain: hsus[h]
   sleep hmimi[h]
@@ -107,5 +115,6 @@ end
 12.times do
   play qlyrics[q]
   sleep qmimi[q]
-  q = q + 1 
+  q = q + 1
 end
+=end
